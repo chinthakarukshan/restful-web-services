90b4069f4b67d7b148c2b1ab6be4099676834019
chinthaka <chinthakarukshan@gmail.com>
2022-06-03 23:27:59 +0530

    Initial commit

---
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.demo.rest.webservices.restfulwebservices.controller;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //@RequestMapping(method= RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello-World!";
+    }
+}
