73da8eb11e0ea5dce537d885ac681e976603b831
chinthaka <chinthakarukshan@gmail.com>
2022-06-03 23:58:58 +0530

    Returning a bean from a Rest API

--- src/main/java/com/demo/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,17 +1,24 @@
 package com.demo.rest.webservices.restfulwebservices.controller;
 
+import com.demo.rest.webservices.restfulwebservices.domain.HelloWorldBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Path;
+
 @RestController
 public class HelloWorldController {
 
-    //@RequestMapping(method= RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello-World!";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello-world-bean");
     }
 }
---
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/domain/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.demo.rest.webservices.restfulwebservices.domain;
+
+public class HelloWorldBean {
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+}
