3a28376dab3f39eeeb74cdd74ad7d9b850dbdb74
chinthaka <chinthakarukshan@gmail.com>
2022-06-07 23:51:01 +0530

    Changes related to create posts

--- src/main/java/com/demo/rest/webservices/restfulwebservices/controller/PostController.java
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/controller/PostController.java
@@ -6,10 +6,12 @@ import com.demo.rest.webservices.restfulwebservices.entity.Post;
 import com.demo.rest.webservices.restfulwebservices.entity.User;
 import com.demo.rest.webservices.restfulwebservices.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +34,20 @@ public class PostController {
         List<Post> userPostList = postDAO.getPosts(userId);
 
         return userPostList;
+    }
+
+    @PostMapping(path = "/users/{userId}/posts")
+    public ResponseEntity<Object> createPost(@PathVariable int userId, @RequestBody Post post) {
+        User user = userDAO.getUser(userId);
+
+        if (user == null) {
+            throw new UserNotFoundException("Id : " + userId);
+        }
+
+        Post createdPost = postDAO.createPost(user,post);
+
+        URI createdUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{postId}").buildAndExpand(createdPost.getId()).toUri();
+
+        return ResponseEntity.created(createdUri).build();
     }
 }
--- src/main/java/com/demo/rest/webservices/restfulwebservices/dao/PostDAO.java
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/dao/PostDAO.java
@@ -1,14 +1,18 @@
 package com.demo.rest.webservices.restfulwebservices.dao;
 
 import com.demo.rest.webservices.restfulwebservices.entity.Post;
+import com.demo.rest.webservices.restfulwebservices.entity.User;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class PostDAO {
-    private static List<Post> userPosts;
+    private static List<Post> userPosts = new ArrayList<>();
+
+    private static AtomicInteger postId = new AtomicInteger(1);
 
     public List<Post> getPosts(int userId) {
         List<Post> userPostList = new ArrayList<>();
@@ -20,5 +24,13 @@ public class PostDAO {
         }
 
         return userPosts;
+    }
+
+    public Post createPost(User user, Post post) {
+        post.setId(postId.incrementAndGet());
+        post.setUser(user);
+        userPosts.add(post);
+
+        return post;
     }
 }
