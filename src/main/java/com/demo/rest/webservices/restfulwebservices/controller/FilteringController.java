fe5179e1edd4db2f1ea95617f2f9c0bf444d9dfd
chinthaka <chinthakarukshan@gmail.com>
2022-06-23 23:52:16 +0530

    Static filtering in a list

--- src/main/java/com/demo/rest/webservices/restfulwebservices/controller/FilteringController.java
+++ src/main/java/com/demo/rest/webservices/restfulwebservices/controller/FilteringController.java
@@ -4,12 +4,20 @@ import com.demo.rest.webservices.restfulwebservices.dto.DemoFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class FilteringController {
 
     @GetMapping("/filtering")
     public DemoFilter getFilter() {
         return new DemoFilter("value1", "value2", "value3");
+    }
+
+    @GetMapping("/filtering-list")
+    public List<DemoFilter> getFilterList() {
+        return Arrays.asList(new DemoFilter("value1", "value2", "value3"),new DemoFilter("value11", "value22", "value33"));
     }
 
 }
